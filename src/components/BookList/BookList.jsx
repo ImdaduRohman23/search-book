bdc40ffea6b9792cfb9f21c2cdca1e4afde0feee
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-11-17 08:01:40 +0700

    add card list book

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import './booklist.css';
-
 const BookList = ({books}) => {
     console.log(books)
 
     return (
-        <div>
-            <h2>Book List</h2>
-            {
-                books.map(book => {
-                    return <>
-                        <h5>
-                            {/* {book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.smallThumbnail} */}
-                            {book.volumeInfo.title}
-                        </h5>
-                    </>
-                })
-            }
+        <div className='bookList'>
+            <div className="bookList-container">
+                {
+                    books.map(book => {
+                        return (
+                            <div className='bookList__card'>
+                                <div className="bookList__card-img">
+                                    <img src={book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.smallThumbnail}  alt="" />
+                                </div>
+                                <div className="bookList__card-title">
+                                    <h6>{book.volumeInfo.title}</h6>
+                                </div>
+                            </div>
+                            )
+                            {/* <h5>
+                                {book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.smallThumbnail}
+                                {book.volumeInfo.title}
+                            </h5>
+                            <div className="bookList__item">
+
+                            </div>
+                        </> */}
+                    })
+                }
+            </div>
         </div>
     )
 }
